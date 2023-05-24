6c411de3e9317163f4133989120c3e728a1a317b
Brian Chandler <47120223+bmchandler@users.noreply.github.com>
2023-05-24 20:23:48 +0000

    Update GHCS setup to include build tooling.

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -3,3 +3,12 @@
 # Trying alternative to doing "postCreateCommand": "bash -i -c 'sudo apt install bat'" in the devcontainer.json
 sudo apt update
 sudo apt -y install bat
+
+# NVM comes pre-installed on this image, so adding install of Node.js for our
+# transpilation tools.
+nvm install --lts
+nvm use --lts
+
+# Install our build tooling (Pug & Sass)
+npm install -g pug-cli
+npm install -g sass
